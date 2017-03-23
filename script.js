c6e3f070e78b21b730cc3d60a3e6328da517ab5b
Gustavo Eumana <gustavo.eumana@student.uncommonhs.org>
2017-03-23 21:47:29 +0000

    my comment

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
+function triangle(){
+forward(100);
+right(120);
+forward(100);
+right(120);
+forward(100);
+
+}
+function rectangle(){
+    forward(100);
+    right(90);
+    forward(50);
+    right(90);
+    forward(100);
+    right(90);
+    forward(50);
+}
 function square(){
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
-right(90);
-forward(50);
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+    forward(100);
+    right(90);
+}
+function hexagon(){
+    for (i = 0; i < 6; i++){
+    forward(50);
+    right(60);
+    }
+   
 }
 
 // Type your function call below
 
+
+hexagon();
 
 stamp();
